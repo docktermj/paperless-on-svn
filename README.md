3532d7f78e189802623794db83c39689ff6b1a3a
docktermj <michael@dockter.com>
2025-09-21 22:54:26 -0400

    #1 Savepoint

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# paperless
+# paperless-on-svn
+
+## Use
+
+1. [svn_changes.json]
+    1. Copy [svn_changes.json] to `~/.config/svn_changes.json`
+    1. Edit `~/.config/svn_changes.json`
+
+1. Run [svn_changes.py]
---
+++ svn_changes.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python3
+
+import json
+import os
+import shutil
+
+import svn.local
+
+CONFIG_PATH = os.path.expanduser("~/.config/svn_changes.json")
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+
+def copy_file(input_path, output_path, source_path):
+    destination_path = source_path.replace(input_path, output_path)
+    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+    shutil.copy(source_path, destination_path)
+
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    # Read configuration file.
+
+    with open(CONFIG_PATH, "r") as file:
+        config = json.load(file)
+
+    input_path = os.path.expanduser(config.get("inputPath"))
+    output_path = os.path.expanduser(config.get("outputPath"))
+    svn_paths = config.get("svnPaths")
+
+    # Process individual SVN repositories.
+
+    errors = 0
+    for svn_path in svn_paths:
+
+        path = svn_path.get("path")
+        old_revision = svn_path.get("revision")
+
+        repository_path = os.path.join(input_path, path)
+        repository = svn.local.LocalClient(repository_path)
+        info = repository.info()
+        new_revision = info.get("entry_revision")
+        svn_path["revision"] = new_revision
+
+        # Determine the files that differ between the old and current revisions.
+
+        diffs = repository.diff_summary(old_revision, new_revision)
+
+        # Process the differences.
+
+        for diff in diffs:
+            item = diff.get("item")
+            kind = diff.get("kind")
+            path = diff.get("path")
+            match item:
+                case "added":
+                    match kind:
+                        case "file":
+                            copy_file(input_path, output_path, path)
+                        case "dir":
+                            continue
+                        case _:
+                            errors += 1
+                            print(f"Unknown kind: {kind}")
+                case _:
+                    errors += 1
+                    print(f"Unknown item: {item}")
+
+    # Write configuration file with updated revisions.
+
+    if errors == 0:
+        with open(CONFIG_PATH, "w") as file:
+            json.dump(config, file, indent=4)
